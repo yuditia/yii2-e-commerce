148fbf396eb648a5d45fd5453fd99f7a8573acb4
yuditia <bunggukechi@gmail.com>
2022-05-16 16:59:47 +0700

    first commit

---
+++ backend/views/site/login.php
@@ -0,0 +1,53 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap4\ActiveForm $form */
+/** @var \common\models\LoginForm $model */
+
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+
+$this->title = 'Login';
+?>
+
+<div class="row">
+    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+        <div class="col-lg-6">
+            <div class="p-5">
+                <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Please Sign in</h1>
+                </div>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'options'=> ['class'=>'user']
+                    ]); ?>
+
+                <?= $form->field($model, 'username',[
+                    'inputOptions' => [
+                        'class' => 'form-control form-control-user',
+                        'placeholder'=> 'Enter Username..'
+                    ]
+                ])->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'password',[
+                    'inputOptions' => [
+                        'class' => 'form-control form-control-user',
+                        'placeholder'=> 'Enter Password..'
+                    ]
+                ])->passwordInput() ?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+                    
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-user btn-block', 'name' => 'login-button']) ?>
+                    <hr>
+                    
+                    <?php ActiveForm::end(); ?>
+                <hr>
+                <div class="text-center">
+                    <a class="small" href="<?php echo \yii\helpers\Url::to(['/site/forgot-password']); ?>">Forgot Password?</a>
+                </div>
+                
+        </div>
+    </div>
+</div>
